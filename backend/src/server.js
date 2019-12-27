82beb1d133b4a9abbc3d64b5d3aac518b61ea611
Breno Lopes <breno.souza.developer@gmail.com>
2019-12-27 18:38:06 -0300

    adicionado o mongoose e connectado com o mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,14 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import routes from './routes'
 
 const app = express()
+
+mongoose.connect('mongodb+srv://reactbox:reactbox@meubanco-tgxc0.mongodb.net/reactbox?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
