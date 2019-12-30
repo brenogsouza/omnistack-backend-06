2771e3031e14324e33d8a1e61687f86ba93bc527
Breno Lopes <breno.souza.developer@gmail.com>
2019-12-30 10:43:42 -0300

    adicionado socket.io e feitos as configurações no server

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,15 @@ import routes from './routes'
 import path from 'path'
 
 const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+// toda vez que receber um conexao do usuário para deixar um usuário em sala específica
+io.on('connection', socket => {
+  socket.on('connectRoom', box => {
+    socket.join(box)
+  })
+})
 
 mongoose.connect('mongodb+srv://reactbox:reactbox@meubanco-tgxc0.mongodb.net/reactbox?retryWrites=true&w=majority',
   {
@@ -11,8 +20,10 @@ mongoose.connect('mongodb+srv://reactbox:reactbox@meubanco-tgxc0.mongodb.net/rea
     useUnifiedTopology: true
   })
 
+app.use((req, res, next) => { req.io = io; return next() })
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/files', express.static(path.resolve(__dirname, '..', 'temp')))
 app.use(routes)
-app.listen(3001)
+
+server.listen(3001)
